4d482be3b71ca724ecdec36bbc71c4336c3ce94c
John Phillip Lee <johnphilliplee@johns-macbook-pro.local>
2018-08-12 17:47:33 +0800

    Initial commit

---
+++ WaitAdvisor/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  WaitAdvisor
+//
+//  Created by John Phillip Lee on 12/08/2018.
+//  Copyright © 2018 John Phillip Lee. All rights reserved.
+//
+
+import UIKit
+
+enum state {
+    case started
+    case stopped
+    case responseNeeded
+}
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+     View   - Handles tap detection, has a callback for when state changed
+            - Handles view appearance on state change
+     
+     */
+    
+    
+}
