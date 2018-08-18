942af2de356eadd87a320d1cffa13a9eddd84933
John Phillip Lee <johnphilliplee@johns-macbook-pro.local>
2018-08-18 15:35:07 +0800

    Implemented UI changes when state changes

--- WaitAdvisor/Views/StateView.swift
+++ WaitAdvisor/Views/StateView.swift
@@ -18,15 +18,17 @@ protocol StateViewModelDelegate: class {
 class StateViewModel: NSObject {
     let model: StateModel
     var stateValue: State
+    var callback: ((State) -> ())?
     var observer: NSObjectProtocol?
     weak var delegate: StateViewModelDelegate?
     
-    init(model: StateModel) {
+    init(model: StateModel, callback: ((State) -> ())? = nil) {
         self.model = model
         stateValue = model.value
         super.init()
         NotificationCenter.default.addObserver(forName: StateModel.stateDidChange, object: nil, queue: nil) {[weak self] (notification) in
-             self?.stateValue = notification.userInfo?[StateModel.stateKey] as? State ?? .stopped
+            self?.stateValue = notification.userInfo?[StateModel.stateKey] as? State ?? .stopped
+            self?.callback?(self?.stateValue ?? .stopped)
         }
     }
     
@@ -34,11 +36,39 @@ class StateViewModel: NSObject {
         model.value = value
         delegate?.stateViewModel(self, didChangeStateTo: value)
     }
+    
+}
+
+private struct StateUISettings {
+    var switchIsOn: Bool
+    var switchText: String
+    var messageText: String
 }
 
 class StateView: UIView {
-    var model: StateViewModel?
+    @IBOutlet weak private var waitSwitch: UISwitch!
+    @IBOutlet weak private var messageLabel: UILabel!
+    @IBOutlet weak private var switchLabel: UILabel!
     
+    var model: StateViewModel? {
+        didSet {
+            model?.callback = {[weak self] (state) in
+                switch state {
+                case .started:
+                    self?.applyUI(StateUISettings(switchIsOn: true,
+                                                  switchText: "Wait Advisor is ON",
+                                                  messageText: "You have activated the Wait Advisor. Please don't forget to turn this OFF as soon as your wait is over."))
+                case .stopped:
+                    self?.applyUI(StateUISettings(switchIsOn: false,
+                                                  switchText: "Wait Advisor is OFF",
+                                                  messageText: "If you are currently waiting in a queue or are parked waiting for your cargo to be loaded or unloaded, please turn the switch on."))
+                case .responseNeeded:
+                    return
+                }
+            }
+        }
+    }
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupStructure()
@@ -54,22 +84,7 @@ class StateView: UIView {
         setupStructure()
     }
     
-}
-
-fileprivate extension StateView {
-    
-    func setupStructure() {
-        let viewFromNib = viewFromOwnedNib()
-        addSubviewAndFill(viewFromNib)
-//        setupGestureRecognizer()
-    }
-    
-    func setupGestureRecognizer() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapped(_:)))
-        self.addGestureRecognizer(tapGesture)
-    }
-    
-    @objc func tapped(_ recognizer: UITapGestureRecognizer) {
+    @IBAction func didChangeValue(_ sender: UISwitch) {
         guard let modelObject = model else {
             return
         }
@@ -78,10 +93,26 @@ fileprivate extension StateView {
         case .stopped:
             newState = .started
         case .started:
-            newState = .responseNeeded
-        case .responseNeeded:
             newState = .stopped
+        default:
+            fatalError()
         }
         model?.commit(value: newState)
+    }
+    
+}
+
+fileprivate extension StateView {
+    
+    func setupStructure() {
+        let viewFromNib = viewFromOwnedNib()
+        addSubviewAndFill(viewFromNib)
+        model?.commit(value: .stopped)
+    }
+    
+    func applyUI(_ setting: StateUISettings) {
+        waitSwitch.isOn = setting.switchIsOn
+        switchLabel.text = setting.switchText
+        messageLabel.text = setting.messageText
     }
 }
